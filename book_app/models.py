33dfba9da63cf332680a93cceeec689cc299dfa8
pych1971 <pych1971@gmail.com>
2024-03-07 12:11:03 +0500

    2024-03-07-12-10

--- book_app/models.py
+++ book_app/models.py
@@ -7,7 +7,8 @@ class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
     is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
 
     def __str__(self):
-        return f'{self.title} - {self.rating}%'
+        return f'{self.title}, {self.author} - {self.rating}%'
