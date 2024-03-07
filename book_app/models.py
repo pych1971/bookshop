d49efcfc3576fdb30417a38818e755f0fedb86fe
pych1971 <pych1971@gmail.com>
2024-03-07 11:35:18 +0500

    2024-03-07-11-35

--- book_app/models.py
+++ book_app/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.title} - {self.rating}%'
